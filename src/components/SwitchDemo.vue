22ad9ced5b1260ec1fe3438432f946d8de6178e0
Eric <1103787680@qq.com>
2020-12-29 18:52:44 +0800

    switch

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,22 +1,23 @@
 <template>
-	<div class="switch-demo">
-		<p>switch</p>
-		<ele-switch/>
-	</div>
+  <div class="switch-demo">
+    <p>switch</p>
+    <ele-switch />
+  </div>
 </template>
 
 <script>
-import Switch from "../lib/switch/Switch.vue"
+import Switch from '../lib/switch/Switch.vue'
 export default {
-	components:{
-			"ele-switch":Switch
-	}
+  components: {
+    'ele-switch': Switch,
+  },
 }
 </script>
 
 <style lang="scss">
-.switch-demo{
-	height: 1500px;
-	background-color: pink;
+.switch-demo {
+  height: 1500px;
+  background-color: pink;
+  padding: 20px;
 }
 </style>
--- src/lib/switch/Switch.vue
+++ src/lib/switch/Switch.vue
@@ -1,16 +1,55 @@
 <template>
-  <div class="switch">
-    <button>
-      开 关
-      <span> </span>
+  <div class="ele-switch">
+    <button @click="toogleVal" :class="{ active: checked }">
+      <!-- 开 关 -->
+      <span :class="{ active: checked }"> </span>
     </button>
   </div>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue'
 export default {
   name: 'Switch',
+  setup(props) {
+    const checked = ref(false)
+    const toogleVal = () => {
+      checked.value = !checked.value
+    }
+    return { checked, toogleVal }
+  },
 }
 </script>
 
-<style lang="scss" scopde></style>
+<style lang="scss">
+.ele-switch {
+  button {
+    width: 52px;
+    height: 22px;
+    background-color: #ddd;
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    overflow: hidden;
+    transition: all 300ms ease-in;
+    &:focus {
+      outline: none;
+    }
+    &.active {
+      background-color: green;
+    }
+    span {
+      display: block;
+      width: 18px;
+      height: 18px;
+      border-radius: 100%;
+      border: 1px solid #fff;
+      background-color: #fff;
+      transform: translateX(1px);
+      transition: all 300ms ease-in;
+      &.active {
+        transform: translateX(calc(50px - 18px));
+      }
+    }
+  }
+}
+</style>
