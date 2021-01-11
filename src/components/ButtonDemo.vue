37cbd25ecaa265f062c492d6790f39f9b764be64
Eric <1103787680@qq.com>
2021-01-11 20:27:22 +0800

    button样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="button-demo">
     <div class="context">
-      <ele-button>你好</ele-button>
+      <ele-button @click="onClick" theme="default">默认按钮</ele-button>
+      <ele-button @click="onClick" theme="primary">主要按钮</ele-button>
+      <ele-button @click="onClick" theme="info">信息按钮</ele-button>
+      <ele-button @click="onClick" theme="warning">警告按钮</ele-button>
+      <ele-button @click="onClick" theme="danger">危险按钮</ele-button>
     </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import Button from '../lib/button/Button.vue'
 import { ref } from 'vue'
 export default {
   components: {
     'ele-button': Button,
   },
-  setup() {},
+  setup() {
+    const onClick = () => {
+      console.log('click')
+    }
+    return { onClick }
+  },
 }
 </script>
 
@@ -23,8 +32,12 @@ export default {
   background-color: pink;
   overflow: auto;
   .context {
+    padding: 30px;
     height: 5000px;
     background-color: pink;
+  }
+  .ele-button.ele-button {
+    margin-left: 10px;
   }
 }
 </style>
--- src/lib/button/Button.vue
+++ src/lib/button/Button.vue
@@ -1,11 +1,63 @@
 <template>
-  <button>
+  <button class="ele-button" :class="`ele-button--${theme}`">
     <slot />
   </button>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: 'default',
+    },
+  },
+  setup(props, context) {
+    const { ...rest } = context.attrs
+    const theme = props.theme
+    return {
+      rest,
+      theme,
+    }
+  },
+}
 </script>
 
-<style></style>
+<style lang="scss">
+.ele-button {
+  position: relative;
+  display: inline-block;
+  box-sizing: border-box;
+  height: 44px;
+  margin: 0;
+  padding: 0 18px;
+  font-size: 16px;
+  line-height: 1.2;
+  text-align: center;
+  border-radius: 2px;
+  cursor: pointer;
+  transition: opacity 0.2s;
+  border: 1px solid #fff;
+  color: #fff;
+  &--default {
+    background-color: #fff;
+    color: black;
+  }
+  &--primary {
+    background-color: #07c160;
+    border-color: #07c160;
+  }
+  &--info {
+    background-color: #1989fa;
+    border-color: #1989fa;
+  }
+  &--warning {
+    background-color: #ff976a;
+    border-color: #ff976a;
+  }
+  &--danger {
+    background-color: red;
+    border-color: red;
+  }
+}
+</style>
