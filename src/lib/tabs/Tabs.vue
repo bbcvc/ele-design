3a0540e4c5edcb9ecdf415c5b9095c9e691180ce
Eric <1103787680@qq.com>
2021-09-21 19:13:58 +0800

    优化tab

--- src/lib/tabs/Tabs.vue
+++ src/lib/tabs/Tabs.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="ele-tabs">
-    <div class="ele-tabs-nav">
+    <div class="ele-tabs-nav" ref="container">
       <div
         class="ele-tabs-nav--item"
         v-for="(item, index) in titles"
         :key="item + index"
         @click="slecte(item)"
         :class="{ selected: item === selected }"
+        :ref="
+          (el) => {
+            if (item === selected) selectedItem = el
+          }
+        "
       >
         {{ item }}
       </div>
+      <div class="ele-tabs-nav--indicator" ref="indicator"></div>
     </div>
     <div class="ele-tabs-content">
       <component
@@ -22,7 +28,7 @@
 </template>
 
 <script lang="ts">
-import { computed } from 'vue'
+import { computed, ref, onMounted, watchEffect } from 'vue'
 import Tab from './Tab.vue'
 export default {
   props: {
@@ -31,15 +37,41 @@ export default {
     },
   },
   setup(props, context) {
+    const selectedItem = ref<HTMLDivElement>(null)
+    const indicator = ref<HTMLDivElement>(null)
+    const container = ref<HTMLDivElement>(null)
     const defaults = context.slots.default()
-    const titles = defaults.map((tag) => {
+    const titles: any[] = defaults.map((tag) => {
       return tag.props.title
     })
-    defaults.forEach((tag) => {
-      if (tag.type !== Tab) {
-        throw new Error('Tabs子标签必须为Tab')
-      }
-    })
+    onMounted(() => {
+      const { width } = selectedItem.value.getBoundingClientRect()
+      indicator.value.style.width = width + 'px'
+
+      const { left: cLeft } = container.value.getBoundingClientRect()
+      const { left: rLrft } = selectedItem.value.getBoundingClientRect()
+      const left = rLrft - cLeft
+      indicator.value.style.left = left + 'px'
+      watchEffect(
+        () => {
+          const { width } = selectedItem.value.getBoundingClientRect()
+          indicator.value.style.width = width + 'px'
+
+          const { left: cLeft } = container.value.getBoundingClientRect()
+          const { left: rLrft } = selectedItem.value.getBoundingClientRect()
+          const left = rLrft - cLeft
+          indicator.value.style.left = left + 'px'
+        },
+        {
+          flush: 'post',
+        }
+      )
+    }),
+      defaults.forEach((tag) => {
+        if (tag.type !== Tab) {
+          throw new Error('Tabs子标签必须为Tab')
+        }
+      })
     const current = computed(() => {
       return defaults.filter((tag) => {
         return tag.props.title === props.selected
@@ -51,7 +83,10 @@ export default {
     return {
       defaults,
       titles,
+      container,
+      indicator,
       slecte,
+      selectedItem,
       current,
     }
   },
@@ -59,8 +94,10 @@ export default {
 </script>
 
 <style lang="scss">
+$blue: #1989fd;
 .ele-tabs {
   .ele-tabs-nav {
+    position: relative;
     display: inline-block;
     border-bottom: 1px solid #ddd;
     min-width: 300px;
@@ -69,9 +106,17 @@ export default {
       padding: 5px 12px;
       cursor: pointer;
       &.selected {
-        color: #1989fd;
-        border-bottom: 2px solid #1989fd;
+        color: $blue;
       }
+    }
+    &--indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      width: 100px;
+      left: 0;
+      bottom: -1px;
+      transition: all 250ms ease-out 0s;
     }
   }
 }
