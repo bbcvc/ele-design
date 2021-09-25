8b4a9371d35df9866a41db23e6991b8afec44ac7
Eric <1103787680@qq.com>
2021-09-25 20:35:23 +0800

    feat:导出lib

---
+++ src/lib/index.ts
@@ -0,0 +1,5 @@
+export {default as Button} from './button/Button.vue';
+export {default as Switch} from './switch/Switch.vue';
+export {default as Tab} from './tabs/Tab.vue';
+export {default as Dialog} from './dialog/Dialog.vue';
+export {openDialog as openDialog} from './dialog/index';
